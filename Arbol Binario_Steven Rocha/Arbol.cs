748620fd2de9aa97e6326241d2ba3d732562e123
Steven <canserberOrama125@gmail.com>
2020-10-24 18:33:46 -0600

    Arbol binario casi finalizado

---
+++ Arbol Binario_Steven Rocha/Arbol.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.ComponentModel;
+
+namespace Arbol_Binario_Steven_Rocha
+{
+    class Arbol
+    {
+        public Nodo raiz;
+
+        Graphics nodo;
+        Font font;
+
+        int coordenadasX = 740;
+        int coordenadasY = 45;
+
+        bool encontrado = false;
+
+        //Sobrecarga de constructores
+        public Arbol()
+        {
+            raiz = null;
+        }
+        public Arbol(Graphics nodo, Font font)
+        {
+            this.nodo = nodo;
+            this.font = font;
+        }
+
+        #region Metodo Insertar Datos
+        public bool InsertarDatos(double total)
+        {
+            Nodo temp = new Nodo();
+
+            temp.total = total;
+            temp.izquierdo = null;
+            temp.derecho = null;
+
+            if (raiz == null)
+            {
+                raiz = temp;
+                temp.nivel = 1;
+                return true;
+            }
+            else
+            {
+                Nodo anterior = null, ant;
+                ant = raiz;
+
+                while (ant != null)
+                {
+                    anterior = ant;
+                    if (total < ant.total)
+                    {
+                        ant = ant.izquierdo;
+                    }
+                    else
+                    {
+                        ant = ant.derecho;
+                    }
+                }
+                if (total < anterior.total)
+                {
+                    temp.nivel++;
+                    anterior.izquierdo = temp;
+                    return true;
+                }
+                else if(total > anterior.total)
+                {
+                    temp.nivel++;
+                    anterior.derecho = temp;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        #endregion
+
+        #region Metodo para dibujar el arbol cada vez que haya cambio 
+        public void MostrarArbol(PaintEventArgs e, Color c)
+        {
+            e.Graphics.Clear(c);
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            nodo = e.Graphics;
+            DibujarArbol(nodo, font, Brushes.White, Brushes.Black, Pens.White, Brushes.Black);
+        }
+        #endregion
+
+        public bool Eliminar(double total)
+        {
+            raiz = EliminarNodo(raiz, total);
+            return encontrado;
+        }
+
+        #region Eliminar el nodo
+        public Nodo EliminarNodo(Nodo Raiz, double total)
+        {
+            if (Raiz == null)
+            {
+                MessageBox.Show("No se ha encontrado el nodo a eliminar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                encontrado = false;
+            }
+            else if (total < Raiz.total)
+            {
+                Nodo izq = EliminarNodo(Raiz.izquierdo, total);
+                Raiz.izquierdo = izq;
+            }
+            else if(total > Raiz.total)
+            {
+                Nodo der = EliminarNodo(Raiz.derecho, total);
+                Raiz.derecho = der;
+            }
+            else
+            {
+                Nodo axu = Raiz;
+
+                if (axu.derecho == null)
+                {
+                    Raiz = axu.izquierdo;
+                }
+                else if(axu.izquierdo == null)
+                {
+                    Raiz = axu.derecho;
+                }
+                else
+                {
+                    axu = Cambiar(axu);
+                }
+                axu = null;
+                encontrado = true;
+            }
+            return Raiz;
+        }
+        #endregion
+
+        #region Cambiar un nodo por otro, al eliminar
+        protected Nodo Cambiar(Nodo axu)
+        {
+            Nodo temp = axu;
+            Nodo temp2 = axu.izquierdo;
+
+            while (temp2.derecho != null)
+            {
+                temp = temp2;
+                temp2 = temp2.derecho;
+            }
+            axu.total = temp2.total;
+
+            if (temp == axu)
+            {
+                temp.izquierdo = temp2.izquierdo;
+            }
+            else
+            {
+                temp.derecho = temp2.izquierdo;
+            }
+            return temp2;
+        }
+        #endregion
+
+        #region Dibujar el arbol
+        public void DibujarArbol(Graphics grafico, Font fuente, Brush colorRelleno, Brush colorFuente, Pen relacion, Brush borde)
+        {
+            if (raiz == null)
+            {
+                return;
+            }
+
+            raiz.UbicacionNodo(coordenadasX, coordenadasY);
+            raiz.DibujarRamas(grafico, relacion);
+            raiz.DibujarNodo(grafico, fuente, colorRelleno, colorFuente, relacion, borde);
+
+        }
+        #endregion
+
+        #region Recorrido InOrden
+        public void InOrden(ListBox lst, Label lbl)
+        {
+            lst.Items.Clear();
+            InOrden(raiz, lst, lbl);
+        }
+        public void InOrden(Nodo temp, ListBox lst, Label lbl)
+        {
+            if (temp != null)
+            {
+                lbl.Text = "Recorrido InOrden";
+                InOrden(temp.izquierdo, lst, lbl);
+                lst.Items.Add(temp.total.ToString());
+                InOrden(temp.derecho, lst, lbl);
+            }
+        }
+        #endregion
+
+        #region Recorrido PosOrden
+        public void PosOrden(ListBox lst, Label lbl)
+        {
+            lst.Items.Clear();
+            PosOrden(raiz, lst, lbl);
+        }
+        public void PosOrden(Nodo temp, ListBox lst, Label lbl)
+        {
+            if (temp != null)
+            {
+                lbl.Text = "Recorrido PosOrden";
+                PosOrden(temp.izquierdo, lst, lbl);
+                PosOrden(temp.derecho, lst, lbl);
+                lst.Items.Add(temp.total.ToString());
+            }
+        }
+        #endregion
+
+        #region Recorrido PreOrden
+        public void PreOrden(ListBox lst, Label lbl)
+        {
+            lst.Items.Clear();
+            PreOrden(raiz, lst, lbl);
+        }
+        public void PreOrden(Nodo temp, ListBox lst, Label lbl)
+        {
+            if (temp != null)
+            {
+                lbl.Text = "Recorrido PreOrden";
+                lst.Items.Add(temp.total.ToString());
+                PreOrden(temp.izquierdo, lst, lbl);
+                PreOrden(temp.derecho, lst, lbl);
+            }
+        }
+        #endregion
+    }
+}
---
+++ Arbol Binario_Steven Rocha/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arbol_Binario_Steven_Rocha
+{
+    public partial class Form1 : Form
+    {
+        //Variables para trabajar. Manejar la cantidad de clientes y el indice
+        public int cantClientes { get; set; }
+
+        int axu,
+            posX,
+            posY,
+            i = 0;
+
+        public double iva = 15;
+
+        //Variable de tipo arbol la cual posteriormente instanciaremos. Y variable de tipo Graphics la cual nos servira para dibujar el arbol
+        Arbol arbol;
+        Graphics nodo;
+
+        //Arreglo de registro para almacenar valores
+        public struct datos
+        {
+            public string cliente { get; set; }
+            public double precio { get; set; }
+            public int dias { get; set; }
+            public double total { get; set; }
+        }
+
+        //Arreglo de registro unidimensional
+        datos[] dato;
+
+        //Constructor de la clase form
+        public Form1()
+        {
+            InitializeComponent();
+            nodo = CreateGraphics();
+            arbol = new Arbol(nodo, Font);
+            gbEliminar.Enabled = false;
+            gbRecorridos.Enabled = false;
+            gbDatos.Enabled = false;
+        }
+
+        #region Metodo Cantidad
+        public void Cantidad()
+        {
+            try
+            {
+                cantClientes = Convert.ToInt32(txtCantClientes.Text);
+                axu = cantClientes;
+
+                if (cantClientes < 0 || txtCantClientes.Text.Length > 8)
+                {
+                    MessageBox.Show("Cantidad ingresada no valida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    dato = new datos[cantClientes];
+                    MessageBox.Show($"Se podran registrar un total de {cantClientes} clientes", ":)", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    gbDatos.Enabled = true;
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Cantidad ingresada no valida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }  
+        }
+        #endregion
+
+        private void btnCantClientes_Click(object sender, EventArgs e)
+        {
+            Cantidad();
+        }
+
+        #region Metodo para insertar nodos/datos
+        public void Insertar()
+        {
+            if (i == cantClientes)
+            {
+                MessageBox.Show("Ya no puede ingresar mas elementos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    dato[i].cliente = txtCliente.Text;
+                    dato[i].precio = double.Parse(txtPrecio.Text);
+                    dato[i].dias = Convert.ToInt32(txtNumDias.Text);
+                    dato[i].total = (((dato[i].precio * dato[i].dias) * iva) / 100) + (dato[i].precio * dato[i].dias);
+
+                    if (!arbol.InsertarDatos(dato[i].total))
+                    {
+                        MessageBox.Show("No se pueden ingresar valores duplicados.\nIngrese su precio nuevamente", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        txtPrecio.Clear();
+                        txtPrecio.Focus();
+                    }
+                    else
+                    {
+                        dataGridView1.Rows.Add(dato[i].cliente, dato[i].total);
+                        Refresh();
+                        LimpiarCampos();
+                        txtCliente.Focus();
+                        i++;
+                    }
+
+                    if (i > 0)
+                    {
+                        gbRecorridos.Enabled = true;
+                    }
+                    if (i == cantClientes)
+                    {
+                        gbEliminar.Enabled = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Datos erroneos.\nRevisar los datos previamente ingresados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        #endregion
+
+        #region Limpiar los campos
+        public void LimpiarCampos()
+        {
+            txtCliente.Clear();
+            txtNumDias.Clear();
+            txtPrecio.Clear();
+            txtParqueo.Clear();
+        }
+        #endregion
+
+        private void btnDatos_Click(object sender, EventArgs e)
+        {
+            Insertar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arbol.InOrden(lstRecorridos, lblRecorridos);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            arbol.MostrarArbol(e, this.BackColor);
+        }
+
+        private void btnPosOrden_Click(object sender, EventArgs e)
+        {
+            arbol.PosOrden(lstRecorridos, lblRecorridos);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPreOrden_Click(object sender, EventArgs e)
+        {
+            arbol.PreOrden(lstRecorridos, lblRecorridos);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+
+            if (arbol.Eliminar(x))
+            {
+                Refresh();
+                Eliminar(x);
+            }
+            else return;
+        }
+
+
+        #region Eliminar del datagridview
+        public void Eliminar(double x)
+        {
+            for (int i = 0; i < dato.Length; i++)
+            {
+                if (dato[i].total == x)
+                {
+                    for (int j = i; j < dato.Length - 1; j++)
+                    {
+                        dato[j].total = dato[j + 1].total;
+                        dato[j].cliente = dato[j + 1].cliente;
+                        dato[j].dias = dato[j + 1].dias;
+
+                        if (j == dato.Length)
+                        {
+                            dato[j].total = Convert.ToDouble(""); ;
+                            dato[j].cliente = null;
+                            dato[j].dias = Convert.ToInt32(null);
+                        }
+                    }
+                }
+            }
+
+            axu--;
+            dataGridView1.Rows.Clear();
+
+            for (int i = 0; i < axu; i++)
+            {
+                dataGridView1.Rows.Add(dato[i].cliente, dato[i].total);
+            }
+        }
+        #endregion
+
+        private void barraNavegacion_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+            {
+                posX = e.X;
+                posY = e.Y;
+            }
+            else
+            {
+                Left += (e.X - posX);
+                Top += (e.Y - posY);
+            }
+        }
+    }
+}
